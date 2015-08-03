d3356474cd1b0ce8271f399ea922b87730513edf
Tristian Flanagan <tflanagan@datacollaborative.com>
2015-08-03 12:34:57 -0400

    Added API_CreateGroup response

--- main.js
+++ main.js
@@ -654,9 +654,15 @@ var actions = {
 		// request: function(context){
 		// 	return Promise.resolve();
 		// },
-		// response: function(context, result){
-		// 	return Promise.resolve(result);
-		// }
+		response: function(context, result){
+			if(result.group){
+				result.group.id = result.group.$.id;
+
+				delete result.group.$;
+			}
+
+			return Promise.resolve(result);
+		}
 	},
 	API_CreateTable: {
 		// request: function(context){
